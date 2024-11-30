45b3b3a5f0b78bff0cea56da0546b5ef2c2aed1a
VINICIUS CHRIST DE CASTRO <viniciusc.castro09@gmail.com>
2024-11-30 17:01:22 -0300

    Update EST_CASO_IV.sql

--- DML/EST_CASO_IV.sql
+++ DML/EST_CASO_IV.sql
@@ -1,6 +1,4 @@
 --Listar todos os alunos matriculados em uma modalidade específica:
-sql
-Copiar código
 SELECT a.nome AS aluno, m.nome AS modalidade
 FROM Matriculas mat
 JOIN Alunos a ON mat.id_aluno = a.id_aluno
@@ -8,16 +6,12 @@ JOIN Modalidades m ON mat.id_modalidade = m.id_modalidade
 WHERE m.nome = 'Musculação';
 
 --Verificar planos de treino e instrutores:
-sql
-Copiar código
 SELECT a.nome AS aluno, p.descricao AS plano, i.nome AS instrutor
 FROM PlanosTreinamento p
 JOIN Alunos a ON p.id_aluno = a.id_aluno
 JOIN Instrutores i ON p.id_instrutor = i.id_instrutor;
 
 --Controlar lotação de uma aula:
-sql
-Copiar código
 SELECT a.data_horario, COUNT(pa.id_aluno) AS total_participantes, a.capacidade
 FROM Aulas a
 LEFT JOIN ParticipantesAulas pa ON a.id_aula = pa.id_aula
@@ -25,8 +19,6 @@ WHERE a.id_aula = 1
 GROUP BY a.id_aula;
 
 --Acompanhar status de pagamentos:
-sql
-Copiar código
 SELECT a.nome AS aluno, p.data_pagamento, p.valor, p.status
 FROM Pagamentos p
 JOIN Alunos a ON p.id_aluno = a.id_aluno;
